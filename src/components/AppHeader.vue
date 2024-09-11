c48b42923d66171e884e0f09c1eafcf035723ced
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-09-11 16:31:49 +0200

    Template

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return { 
+      message:'Template Vite + Vue'
+    }
+  },
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+header{
+  background-color: aquamarine;
+  text-align: center;
+}
+h1 {
+  color: black;
+}
+</style>
